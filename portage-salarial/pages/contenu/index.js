5661c0c72535d4a6997559ffe8549c38157d884d
OunifiSamir <79377613+OunifiSamir@users.noreply.github.com>
2022-03-06 02:03:08 +0100

    pim integration backend with template

--- portage-salarial/pages/contenu/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Contenu = () => {
-  return <div><h3>ici le page index de contenu</h3></div>;
-};
-
-export default Contenu;
--- portage-salarial/pages/contenu/post.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const post = () => {
-  return <div><h2>POST</h2></div>;
-};
-
-export default post;
